697d075f559b08ab049bb23d28d558f2fa1854eb
Caesar Dapiton <dapitoncaesar26@gmail.com>
2023-02-03 07:48:32 +0800

    With a do while

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace cs_proj001;
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        int [] num1  = {1, 2, 4, 5, 7, 13, 56};
+
+        int tryAgain = 0;
+
+        do{
+
+        Console.Write("Enter First Name: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Enter Last Name: ");
+        string lastName = Console.ReadLine();
+        
+        try{
+        Console.Write("Press [1] to try again: ");
+        tryAgain = Convert.ToInt16(Console.ReadLine());
+        
+
+        }catch(Exception e){
+            Console.WriteLine(e.Message);
+        }
+
+        
+
+        }while(tryAgain==1);
+
+        Console.WriteLine("You got out I guess");
+
+    
+
+
+
+
+
+
+
+
+    }
+
+}
