06ea4e12854acd1e434c52d15490c5bb365de0c1
caesar0016 <94184309+caesar0016@users.noreply.github.com>
2023-02-06 22:45:46 +0800

    c# switch method

---
+++ main.cs
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Menu:);
+    Console.WriteLine("[1] Register a number:");
+    Console.WriteLine("[2] Register Names");
+    Console.WriteLine("[3] Address:");
+    Console.WriteLine("[4] Parents:");
+    Console.WriteLine("[5] Subjects:");
+    
+    int num1;
+    
+    switch(num1){
+        case 1:
+        Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+        Console.WriteLine("|                                 |");
+        Console.WriteLine("|        Register a Number        |");
+        Console.WriteLine("|                                 |");
+        Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+        
+        Console.WriteLine("[1] to generate a number");
+    }
+        case 2:
+        Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+        Console.WriteLine("|                                 |");
+        Console.WriteLine("|        Register Names           |");
+        Console.WriteLine("|                                 |");
+        Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+        
+        Console.WriteLine("Enter First Name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Enter Middle Name: ");
+        string middleName = Console.ReadLine();
+        Console.WriteLine("Enter Last Name: ");
+        string lastName = Console.ReadLine();
+        
+    
+    
+    
+  }
+}
